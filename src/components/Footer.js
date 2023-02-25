f2b05b9f2b378500d3af00bf7a8f323066da8ff5
srini047 <srinikethcr7@gmail.com>
2023-02-25 15:30:45 +0530

    added: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,14 @@
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React from 'react';
+import './Footer.css';
 
-export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
+function Footer() {
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
+    <footer className="footer">
+      <div className="container">
+        <p>Made in 💜 @<span><a href='https://github.com/srini047/' target="_blank">srini047</a></span></p>
+      </div>
+    </footer>
   );
 }
+
+export default Footer;
